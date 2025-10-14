edcbe08ceab07aa290aaaacdd52b43aff6a09a89
Johelagr29 <johel.agr.20629@gmail.com>
2025-10-13 22:52:19 -0600

    Update main
    -Se agregan ciclos para generar cantidades de datos random

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 
 
-print("Hello World!") 
-print("Hello World!") 
-print("Hello 
+import random 
+
+placas = []
+
+for i in range(2):
+    placa = random.randint(100000, 999999)
+    placas.append(placa)
+
+print(placas, '\n')
+
+hora_llegada = []
+
+for i in range(2):
+    hora = random.randint(0000, 2359)
+    hora_llegada.append(str(hora).zfill(4))
+
+print(hora_llegada, '\n')
+
+opciones = {
+
+'Sedan': 3, 
+'SUV': 3, 
+'Ambulancia': 1, 
+'Motocicleta': 2,
+'Camion': 2,
+'Policia': 1,
+'Bomberos': 1,
+'Bicicleta': 2 
+}
+
+tipos_de_carro = []
+
+for i in range(2):
+    vehiculo = random.choice(list(opciones.keys()))
+    prioridad = opciones[vehiculo]
+    tipos_de_carro.append([vehiculo, prioridad])
+
+print(tipos_de_carro, '\n') 
 
 
+combinaciones = []
 
+for carro in range(len(tipos_de_carro)):
+    for placa in range(len(placas)):
+        for hora in range(len(hora_llegada)):
+            combinaciones.append([tipos_de_carro[carro], placas[placa], hora_llegada[hora]])
 
-<<<<<<< Updated upstream
-=======
-alguna putada
-nbnb
-hh
-hghghg
-lklklklklkkklklkl
-klklk
-lkkjkjlkjlkj
-klkjlkjl
-ujhhgh
-gfjgjgh
->>>>>>> Stashed changes
+print(combinaciones)
+
