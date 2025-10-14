c66691186b1ddf8bbb1db5e7f7599f605e13932a
dannyromans13 <dannyromans13@gmail.com>
2025-10-13 21:52:12 -0600

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,22 @@
+
+
+print("Hello World!") 
+print("Hello World!") 
+print("Hello 
+
+
+
+
+<<<<<<< Updated upstream
+=======
+alguna putada
+nbnb
+hh
+hghghg
+lklklklklkkklklkl
+klklk
+lkkjkjlkjlkj
+klkjlkjl
+ujhhgh
+gfjgjgh
+>>>>>>> Stashed changes
